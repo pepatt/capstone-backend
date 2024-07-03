97db15ee27a922e549ecd3bb8615058b80ac7eb6
Tymur Rezaei <rezai.timur.04@gmail.com>
2024-07-02 20:34:27 -0700

    set up backend with .env and router

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import router from './routes/main.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config(); 
+
+const PORT = process.env.PORT || 5050;
+
+const app = express();
+//middleware
+app.use(cors());
+app.use(express.json())
+
+app.use("/main", router)
+
+
+//listening
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
