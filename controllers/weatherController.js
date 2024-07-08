8935207aae00a241e2833a2c9888d13a9b6625c3
Tymur Rezaei <rezai.timur.04@gmail.com>
2024-07-07 22:08:04 -0700

    add toggle for applied and notApplied

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -11,7 +11,7 @@ const index = async (req, res) => {
     res.send(usersData);
 }
 
-const insert = async (req, res) => {
+const notApplied = async (req, res) => {
     try {
         
         const weatherResp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${process.env.VITE_BC_VAN_LAT}&lon=${process.env.VITE_BC_VAN_LON}&units=metric&appid=${process.env.VITE_OPEN_WEATHER_API_KEY}`);
@@ -33,7 +33,31 @@ const insert = async (req, res) => {
     }
 }
 
+const applied = async (req, res) => {
+    try {
+        
+        const weatherResp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${process.env.VITE_BC_VAN_LAT}&lon=${process.env.VITE_BC_VAN_LON}&units=metric&appid=${process.env.VITE_OPEN_WEATHER_API_KEY}`);
+        const UVResp = await axios.get(`https://currentuvindex.com/api/v1/uvi?latitude=${process.env.VITE_BC_VAN_LAT}&longitude=${process.env.VITE_BC_VAN_LON}`)
+        
+        const insertResp = await knex("weather").insert({
+            description: weatherResp.data.weather[0].description,
+            temperature: weatherResp.data.main.temp,
+            UVI: UVResp.data.now.uvi,
+            country: weatherResp.data.sys.country,
+            city: weatherResp.data.name,
+            isApplied: true
+        })
+
+        const returnData = await knex.select("*").from("weather").where({"id": insertResp[0]});
+
+        res.send(returnData);
+    } catch (err) {
+        res.status(401).send(`Error occurred: ${err}`)
+    }
+}
+
 export {
     index,
-    insert
+    notApplied,
+    applied
 }
--- routes/weatherRouter.js
+++ routes/weatherRouter.js
@@ -3,7 +3,8 @@ import * as weatherController from '../controllers/weatherController.js';
 const router = express.Router();
 
 router.route("/").get(weatherController.index);
-router.route("/insert").get(weatherController.insert);
+router.route("/notApplied").get(weatherController.notApplied);
+router.route("/applied").get(weatherController.applied);
 
 
 export default router;
